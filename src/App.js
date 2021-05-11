c3b84fe6217c06447f93cf987149de717e704bb8
saideep0531 <79899127+saideep0531@users.noreply.github.com>
2021-05-11 08:59:34 +0530

    imdb

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Login from "./Login";
+import Watchlist from "./Watchlist";
+import Movies from "./Movies";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './Home'
+function App() {
+  return (
+    
+    <Router>
+      <div className="App">
+        <Header />
+           <Route path="/login" component={Login}/>
+            <Route path="/watchlist" component={Watchlist}/>
+            <Route path="/movies" component={Movies}/>
+             <Route  exact path ="/" component ={Home}/>
+                   
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
